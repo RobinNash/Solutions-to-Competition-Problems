b7a653b592e4e2808259dc87dcad98fde3fce5b4
Robin Nash <84207317+RobinNash@users.noreply.github.com>
2021-06-18 23:54:18 -0400

    Create CCC_Next_In_Line_13_J1.cpp

---
+++ C++/CCC_Next_In_Line_13_J1.cpp
@@ -0,0 +1,16 @@
+//CCC Next In Line 13 J1
+//May 14, 2021
+//By Robin Nash
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b, c;
+	cin >> a;
+	cin >> b;
+	c = b + (b-a);
+	cout << c;
+	return 0;
+}
+//1621030125.8399487
